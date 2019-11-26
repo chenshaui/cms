2c65b66379c333e9e83449e1992bdb69fa7e428c
chenshuai <3436263236@qq.com>
2019-11-26 11:14:40 +0800

    修改栏目管理

--- src/main/java/com/briup/cms/web/controller/CategoryController.java
+++ src/main/java/com/briup/cms/web/controller/CategoryController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class CategoryController {
         return MessageUtil.success();
     }
     @ApiOperation("删除栏目")
-    @PostMapping("/delete")
+    @GetMapping("/delete")
     @ApiImplicitParam(name = "id", required = true, paramType = "query",dataType = "int",value = "栏目id")
     public Message deleteCategory(int id){
         categoryService.deleteCategory(id);
@@ -43,14 +44,14 @@ public class CategoryController {
     }
 
     @ApiOperation("查询栏目")
-    @PostMapping("/queryById")
+    @GetMapping("/queryById")
     @ApiImplicitParam(name = "id", required = true, paramType = "query",dataType = "int", value = "栏目id")
     public Message<Category> queryById(int id){
         Category category = categoryService.queryById(id);
         return MessageUtil.success(category);
     }
     @ApiOperation("查询所有栏目")
-    @PostMapping("/findAll")
+    @GetMapping("/findAll")
     public Message<List<Category>> findAll(){
         List<Category> categories = categoryService.findAll();
         return MessageUtil.success(categories);
