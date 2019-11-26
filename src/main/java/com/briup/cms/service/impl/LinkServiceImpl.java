a28ca7d61d8bb26211620b6b831c34ae26181085
chenshuai <3436263236@qq.com>
2019-11-26 09:34:39 +0800

    完成链接管理

--- src/main/java/com/briup/cms/service/impl/LinkServiceImpl.java
+++ src/main/java/com/briup/cms/service/impl/LinkServiceImpl.java
@@ -8,6 +8,8 @@ import com.briup.cms.util.CodeUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class LinkServiceImpl implements ILinkService {
     @Autowired
@@ -16,9 +18,37 @@ public class LinkServiceImpl implements ILinkService {
 
     @Override
     public void addLink(Link link) throws CustomerException {
-        if(link == null){
+        if (link == null){
             throw new CustomerException(CodeUtil.BAD_CODE, "参数为空");
         }
         linkDao.save(link);
     }
+
+    @Override
+    public void deleteLink(int id) throws CustomerException {
+
+        linkDao.deleteById(id);
+    }
+
+
+    public void updateLink(Link link) throws CustomerException {
+        if (link == null){
+           throw new CustomerException(CodeUtil.BAD_CODE, "参数不存在");
+        }
+        linkDao.saveAndFlush(link);
+    }
+
+    @Override
+    public Link queryById(int id) throws CustomerException {
+        Link link = linkDao.queryById(id);
+        return link;
+    }
+
+    @Override
+    public List<Link> findAll() throws CustomerException {
+        List<Link> links = linkDao.findAll();
+        return links;
+    }
+
+
 }
--- src/main/java/com/briup/cms/web/controller/LinkController.java
+++ src/main/java/com/briup/cms/web/controller/LinkController.java
@@ -5,20 +5,51 @@ import com.briup.cms.service.ILinkService;
 import com.briup.cms.util.Message;
 import com.briup.cms.util.MessageUtil;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/link")
 @Api(description = "链接管理")
 public class LinkController {
-   private ILinkService linkService;
-    @PatchMapping("/add")
-    @ApiOperation("添加链接")
+    @Autowired
+    private ILinkService linkService;
+    @PostMapping("/add")
+    @ApiOperation("添加更新S链接")
     public Message addLink(Link link){
         linkService.addLink(link);
         return MessageUtil.success();
+    }
+
+    @GetMapping("/delete")
+    @ApiOperation("删除链接")
+    @ApiImplicitParam(name = "链接id" ,paramType = "query", required = true)
+    public Message deleteLink(int id){
+        linkService.deleteLink(id);
+        return MessageUtil.success();
+    }
+
+    @PostMapping("/update")
+    @ApiOperation("更新链接")
+    public Message updateLink(Link link){
+        linkService.addLink(link);
+        return MessageUtil.success();
+    }
+    @GetMapping("/query")
+    @ApiOperation("查询链接")
+    public  Message<Link> queryById(int id){
+        Link link = linkService.queryById(id);
+        return  MessageUtil.success(link);
+
+    }
+    @GetMapping("/findAll")
+    @ApiOperation("查询所有链接")
+    public Message<List<Link>> findAll(){
+        List<Link> links = linkService.findAll();
+        return MessageUtil.success(links);
     }
 }
